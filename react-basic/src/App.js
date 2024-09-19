a9177527ae10c20ffd46aa6d2fed51f621f45b41
LHFloveXCF <826639991@qq.com>
2024-09-19 22:27:51 +0800

    创建react的骨架

---
+++ react-basic/src/App.js
@@ -0,0 +1,11 @@
+
+// 这是项目的根组件 app -> index.js -> public/index.html(root)
+function App() {
+  return (
+    <div className="App">
+      this is App
+    </div>
+  );
+}
+
+export default App;
---
+++ react-basic/src/index.js
@@ -0,0 +1,10 @@
+// 项目启动必须的包
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// 导入项目入口文件
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
