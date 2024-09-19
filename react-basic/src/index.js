ee0b58f260804241fb9069545c487c71130d57ff
LHFloveXCF <826639991@qq.com>
2024-09-19 22:39:17 +0800

    jsx的应用场景

--- react-basic/src/index.js
+++ react-basic/src/index.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // 导入项目入口文件
-import App from './App';
+// import App from './App';
+import JsxTest from './jsx/jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    // <App />
+    <JsxTest></JsxTest>
 );
---
+++ react-basic/src/jsx/jsx.js
@@ -0,0 +1,28 @@
+{/**jsx的应用场景、领域 */}
+
+const count = 1
+
+function getName() {
+    return '我是一个方法'
+}
+
+function JsxTest() {
+    return (
+        <div className="Jsx">
+            this is Jsx <br></br>
+            {/** Jsx可以用来引用字符串*/}
+            {'this is a message'}<br></br>
+            {/** Jsx可以用来使用变量*/}
+            {count}<br></br>
+            {/** Jsx可以用来调用方法*/}
+            {getName()}<br></br>
+            {/** Jsx可以用来调用函数*/}
+            {new Date().getDate()} <br></br>
+            {/** Jsx可以用来调用js对象*/}
+            {<div color='red'>我是一个div</div>}
+
+        </div>
+    );
+}
+
+export default JsxTest;
